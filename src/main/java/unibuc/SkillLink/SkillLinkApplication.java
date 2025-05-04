825f06d071ffa1895898deda3f3dac2890312f95
radumocanu1 <radumocanu15@gmail.com>
2025-05-04 15:15:02 +0300

    project initialization

---
+++ src/main/java/unibuc/SkillLink/SkillLinkApplication.java
@@ -0,0 +1,13 @@
+package unibuc.SkillLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unibuc/SkillLink/SkillLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package unibuc.SkillLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
